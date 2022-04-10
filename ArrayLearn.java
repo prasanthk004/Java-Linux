9b19074a2a15a5877a1f1c101cc900a0971378ff
prasanthk004 <prasanthkishore004@gmail.com>
2022-04-10 20:02:14 +0530

    Modified previous files

--- ArrayLearn.java
+++ ArrayLearn.java
@@ -9,9 +9,63 @@ public class ArrayLearn {
 		 ArrayLearn a  = new ArrayLearn();
 		// a.InputToArray();
 		// a.DisplayArray();
-		 System.out.println(args.length);
-		 a.DisplayArray1();
+		 
+		// a.DisplayArray1();
+		 a.FindMax();
 	}
+	 
+	 private void FindMax() {
+			// TODO Auto-generated method stub
+			int[] marks= {5,80,45,80,5};
+			
+			int max=-2147483648,  smax= -2147483648,min=2147483647,smin=2147483647;
+			int i ;
+			for(i=0;i<marks.length;i++)
+			{
+				if(marks[i]>max)
+				{
+					smax=max;
+					max=marks[i];
+				}
+				else if(marks[i]>smax&&marks[i]!=max)
+				{
+					smax=marks[i];
+				}
+				
+				if(marks[i]<min)
+				{
+					smin=min;
+					min=marks[i];
+					
+				}
+				else if(marks[i]<smin)
+				{
+					smin=marks[i];
+				}
+				
+				 
+			}
+			//Finding second maximum & minimum
+			
+			/*for(i=0;i<marks.length;i++)
+			{
+				if(marks[i]>smax&&marks[i]!=max)
+				{
+					System.out.println(marks[i]);
+					smax=marks[i];
+				}
+				
+				
+				
+				 
+			}*/
+			
+			System.out.println("First Maximum Mark   : "+max);
+			System.out.println("Second Maximum Mark  : "+smax);
+			System.out.println("Minimum Mark         : "+min);
+			System.out.println("Second Minimum Mark  : "+smin);
+	 }
+	 
 
 	private void DisplayArray1() {
 		// TODO Auto-generated method stub.
