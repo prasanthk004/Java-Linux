52a8d6e0d69407b44f66f3b24fc7b6d88bdc4f1a
prasanthk004 <prasanthkishore004@gmail.com>
2022-04-10 21:20:24 +0530

    Crated Integer Test function on branch newFeatures

--- WrapperPractice.java
+++ WrapperPractice.java
@@ -2,4 +2,21 @@ package tamilnadu.chennai;
 
 public class WrapperPractice {
 
+	
+	int a ;
+	
+	public static void main(String[] args) {
+		WrapperPractice wp= new WrapperPractice();
+		wp.IntegerTest();
+	}
+
+	private void IntegerTest() {
+		// TODO Auto-generated method stub
+		int i=10 ;
+	}
+	
+	
+	
+	
+	
 }
